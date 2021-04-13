ac6ec38b1dfea1c8678e5e388896193275dba69b
danielle076 <d.vandenakker@novi-education.nl>
2021-04-13 20:34:18 +0200

    start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/Signup">Sign up</Link>
+                        </li>
+                        <li>
+                            <Link to="/Signin">Sign in</Link>
+                        </li>
+                        <li>
+                            <Link to="/Profile">Profile</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route exact path="/">
+                        <Home/>
+                    </Route>
+                    <Route path="/Signup">
+                        <SignUp/>
+                    </Route>
+                    <Route path="/Signin">
+                        <SignIn/>
+                    </Route>
+                    <Route path="/Profile">
+                        <Profile/>
+                    </Route>
+                    <Route path="/">
+                        <h1> 404 not found </h1>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
-
-export default App;
